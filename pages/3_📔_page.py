dd702eb67384a3e140a53be7478b8ba844480585
uzairansari0 <109786747+uzairansari0@users.noreply.github.com>
2023-03-14 17:47:32 +0530

    Update 3_📔_page.py

--- pages/3_📔_page.py
+++ pages/3_📔_page.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Sales Dashboard",
                    page_icon=":bar_chart:",
                    layout="wide")
 
-@st.cache_data 
+@st.experimental_memo 
 def get_data_from_excel():
     df = pd.read_excel(
         io="supermarkt_sales.xlsx",
